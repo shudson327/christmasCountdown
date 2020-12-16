ffe35beb26959fcc06d24a649ce737dd0818fa3d
shudson327 <72040252+shudson327@users.noreply.github.com>
2020-12-15 21:32:44 -0500

    pushh

---
+++ christmas.js
@@ -0,0 +1,21 @@
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Dec 25, 2020 00:00:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+
+      document.getElementById('days').innerHTML = Math.floor(distance / (day)),
+        document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
+        });       
+
+    
+    
+        
